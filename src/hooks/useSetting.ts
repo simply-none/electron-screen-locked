b73ec11ef7896476d904e7fa17361c209ef41a23
simply-none <jousindea@163.com>
2024-11-03 22:01:06 +0800

    feat: 增加开机自启动功能

--- src/hooks/useSetting.ts
+++ src/hooks/useSetting.ts
@@ -7,18 +7,22 @@ export default function useSetting() {
   // 其他设置
   const storeAppInnerColor = window.ipcRenderer.sendSync('get-store', 'appInnerColor') || '#ffffff'
   const storeAppBgColor = window.ipcRenderer.sendSync('get-store', 'appBgColor') || '#d4d4d4'
+  // 系统设置
+  const storeIsStartup = window.ipcRenderer.sendSync('get-store', 'isStartup') || false
 
   // 同步设置到缓存
   window.ipcRenderer.sendSync('set-store', 'forceWorkTimes', storeForceWorkTimes);
   window.ipcRenderer.sendSync('set-store', 'todayForceWorkTimes', storeTodayForceWorkTimes);
   window.ipcRenderer.sendSync('set-store', 'appInnerColor', storeAppInnerColor);
   window.ipcRenderer.sendSync('set-store', 'appBgColor', storeAppBgColor);
+  window.ipcRenderer.sendSync('set-store', 'isStartup', storeIsStartup)
 
   // 引用设置
   const forceWorkTimes = ref(window.ipcRenderer.sendSync('get-store', 'forceWorkTimes'))
   const todayForceWorkTimes = ref(window.ipcRenderer.sendSync('get-store', 'todayForceWorkTimes'))
   const appInnerColor = ref(window.ipcRenderer.sendSync('get-store', 'appInnerColor') || '#ffffff')
   const appBgColor = ref(window.ipcRenderer.sendSync('get-store', 'appBgColor') || '#d4d4d4')
+  const isStartup = ref(window.ipcRenderer.sendSync('get-store', 'isStartup') || false)
 
   function setForceWorkTimes(value: number) {
     forceWorkTimes.value = value
@@ -35,6 +39,12 @@ export default function useSetting() {
     window.ipcRenderer.sendSync('set-store', 'appInnerColor', value)
   }
 
+  function setIsStartup (value: boolean) {
+    isStartup.value = value
+    window.ipcRenderer.sendSync('set-store', 'isStartup', value)
+    window.ipcRenderer.send('set-startup', value)
+  }
+
   function setAppBgColor (value: string) {
     console.warn(value)
     appBgColor.value = value
@@ -42,6 +52,7 @@ export default function useSetting() {
   }
 
   return {
+    isStartup,
     forceWorkTimes,
     todayForceWorkTimes,
     setForceWorkTimes,
@@ -49,6 +60,7 @@ export default function useSetting() {
     appInnerColor,
     appBgColor,
     setAppInnerColor,
-    setAppBgColor
+    setAppBgColor,
+    setIsStartup,
   }
 }
